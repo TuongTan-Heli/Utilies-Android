05538570d9d009cc5d385d5463b9a6d6d8377ce2
helijacky <helijackybu@gmail.com>
2025-06-12 11:26:45 +1000

    Fix update task and add dockerfile

--- src/api/taskApi.ts
+++ src/api/taskApi.ts
@@ -4,7 +4,7 @@ export const add = async (UserId: string, Name: string, Description: string, Typ
     return await api.post('/add-task', { UserId, Name, Description, Type, Deadline, EnableNoti, Priority, NotiOnDeadline , Price, CurrencyId});
 };
 
-export const update = async (TaskId: string, Name: string, Description: string, Type: string, Deadline: Date, Done: Date, EnableNoti: boolean, Priority: number, NotiOnDeadline: number, Price: number, CurrencyId: string) => {    
+export const update = async (TaskId: string, Name: string, Description: string, Type: string, Deadline: Date, Done: any, EnableNoti: boolean, Priority: number, NotiOnDeadline: number, Price: number, CurrencyId: string) => {    
     return await api.put('/update-task/' + TaskId, {Name, Description, Type, Deadline, Done, EnableNoti, Priority, NotiOnDeadline , Price, CurrencyId});
 };
 
